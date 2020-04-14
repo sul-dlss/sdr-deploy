9de459a3fcd0d256438a8cf8d39a4f5750d438e0
Justin Coyne <jcoyne@justincoyne.com>
2020-04-14 12:08:52 -0500

    Deploy everything

---
+++ README.md
@@ -0,0 +1,10 @@
+# SDR Deployment script
+
+This is a central place for deploying SDR applications.  This allows all applications
+to be deployed at once.
+
+## Usage
+
+```
+./deploy.rb stage
+```
---
+++ deploy.rb
@@ -0,0 +1,63 @@
+#!/usr/bin/env ruby
+
+# Usage:
+# ./deploy.rb stage
+
+require 'fileutils'
+
+WORK_DIR = ['tmp/repos']
+REPOS = ['sul-dlss/argo', 'sul-dlss/dor-services-app']
+
+def update_repo(repo_dir)
+  Dir.chdir(repo_dir) do
+    `git checkout master 2> /dev/null && git pull`
+  end
+end
+
+def create_repo(repo_dir, repo)
+  FileUtils.mkdir_p repo_dir
+  puts "creating #{repo}"
+  Dir.chdir(repo_dir) do
+    `git clone https://github.com/#{repo}.git .`
+  end
+end
+
+def update_or_create_repo(repo_dir, repo)
+  if File.exist? repo_dir
+    update_repo(repo_dir)
+  else
+    create_repo(repo_dir, repo)
+  end
+end
+
+def deploy(stage)
+  IO.popen("cap #{stage} deploy") do |f|
+    while true
+      begin
+        puts f.readline
+      rescue EOFError
+        break
+      end
+    end
+  end
+end
+
+stage = ARGV[0]
+unless %w[stage qa prod].include?(stage)
+  warn "Usage:"
+  warn "  #{$0} <stage>"
+  warn "\n  stage must be one of \"stage\",\"qa\",\"prod\"\n\n"
+  exit
+end
+
+REPOS.each do |repo|
+  repo_dir = File.join(WORK_DIR, repo)
+  update_or_create_repo(repo_dir, repo)
+  Dir.chdir(repo_dir) do
+    puts "Deploying #{repo_dir}"
+    `bundle install`
+    # Comment out where we ask what branch to deploy. We always deploy master.
+    `sed -i '' 's/^\\(ask :branch.*\\)/#\\1/g' config/deploy.rb`
+    deploy(stage)
+  end
+end
