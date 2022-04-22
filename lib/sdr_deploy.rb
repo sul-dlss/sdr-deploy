b42a3f9c2afaf33aacbe1e28dceb1d021d30e1e1
Michael J. Giarlo <mjgiarlo@stanford.edu>
2022-04-22 15:59:01 -0700

    Parallelize deploys
    Fixes #128
    
    This commit uses the `parallel` gem to run deploys in parallel.

--- lib/sdr_deploy.rb
+++ lib/sdr_deploy.rb
@@ -16,6 +16,8 @@ Config.load_and_set_settings(
 
 # Common methods
 def within_project_dir(repo:, environment: nil, &block)
+  results = []
+
   Dir.chdir(RepoUpdater.new(repo: repo).repo_dir) do
     # NOTE: This is how we execute commands in the project-specific bundler
     #       context, rather than sdr-deploy's bundler context. We want *most* of
@@ -25,12 +27,14 @@ def within_project_dir(repo:, environment: nil, &block)
     contribsys_credentials = ENV['BUNDLE_GEMS__CONTRIBSYS__COM']
     Bundler.with_unbundled_env do
       ENV['BUNDLE_GEMS__CONTRIBSYS__COM'] = contribsys_credentials
-      block.call(environment)
+      results << block.call(environment)
 
       # Some of our apps use non-standard envs and by convention we deploy these when deploying QA
-      return unless environment == 'qa' && repo.non_standard_envs
+      return results unless environment == 'qa' && repo.non_standard_envs
 
-      repo.non_standard_envs.each { |env| block.call(env) }
+      repo.non_standard_envs.each { |env| results << block.call(env) }
+
+      results
     end
   end
 end
